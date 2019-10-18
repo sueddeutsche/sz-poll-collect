31e54da10f4a2bee0a354f56bdead6f3f6a1c3f2
Christian Endt <c.endt@sueddeutsche.de>
2019-10-18 14:45:38 +0200

    Wahlrecht Cleaning improved

--- src/scraper/get_wahlrechtde.R
+++ src/scraper/get_wahlrechtde.R
@@ -6,12 +6,24 @@ df.wahlrecht %>%
 
 source("src/scraper/get_wahlrechtde_bundestag.R")
 
-# df.wahlrecht %>% 
-#   mutate(
-#     Institut = str_remove(Institut,".htm"),
-#     Datum = dmy(Datum)
-#     ) %>% 
-#   filter(
-#     !is.na(Datum),
-#     !str_detect(Befragte, "wahl")
-#     )
+rm(t,table,i,ilinks,institute,j,jlinks,k,url)
+
+df.wahlrecht %>% 
+  unique() ->
+  df_wahlrecht
+
+rm(df.wahlrecht)
+
+df.wahlrecht %>%
+  mutate(
+    Institut = str_remove(Institut,".htm"),
+    Datum = dmy(Datum)
+    ) %>%
+  filter(
+    !is.na(Datum),
+    !str_detect(Befragte, "wahl")
+    ) %>% 
+  View()
+
+
+
